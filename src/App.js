b9f9166802b2b6f24f866ed03000b3855e108b1f
Nadav Shaham <nadavshaham@carmel6000.amitnet.org>
2021-02-21 16:31:18 +0200

    erawe

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> test adsadsa
+          Edit <code>src/App.js</code> sadfsasadasdsa
         </p>
         <a
           className="App-link"
