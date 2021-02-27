7f74df084c55ced0bb24561d0951a0693a78107a
Nadav Shaham <nadavshaham@carmel6000.amitnet.org>
2021-02-27 12:08:24 +0200

    test 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> nadav private
+          Edit <code>src/App.js</code> nadav private asdfsadf
         </p>
         <a
           className="App-link"
