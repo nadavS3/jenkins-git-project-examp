f3d9d38e7013a11db393751c84104eb5baa645be
nadavS3 <nadavshaham@carmel6000.amitnet.org>
2021-09-14 12:50:42 +0300

    added wxios request

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> UPDATEdsafdsafa asdf asdf asdfdsa asdfsad
-        </p>
+        <div onClick={async()=>{
+          let res = await axios.get("/api/organization/all")
+          console.log(res);
+        }} >
+          click to check server
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
